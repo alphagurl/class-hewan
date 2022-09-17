774d6a6c9cc2bd31191d3ba96cb0f5ee8230b002
Haura Adelia Melvandri <102033602+hauraaadeliamelvandri@users.noreply.github.com>
2022-09-18 01:48:50 +0700

    Add files via upload

---
+++ MainApp2.java
@@ -0,0 +1,9 @@
+public class MainApp2 {
+
+    public static void main(String[] args) {
+        Anjing a = new Anjing();
+
+        a.setMata("tajam");
+        System.out.println("Mataku sangat" + a.getBulu());
+    }
+}
